b7dc6e4bfc7b02acd13ae164bf271145083f7a5b
lorijr <jrsilva.lorivas@gmail.com>
2026-08-19 17:49:14 -0300

    algumas assertivas

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,7 +1,10 @@
 import com.viratech.Calculadora;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculadoraTest {
 
@@ -12,5 +15,26 @@ public class CalculadoraTest {
         int resultado = calculadora.soma(2,5);
 
         assertEquals(7, resultado);
+        assertTrue(calculadora.soma(2,5) == 7);
+    }
+
+    @Test
+    public void assertivas(){
+        assertEquals("Casa", "Casa");
+        assertNotEquals("Casa", "casa");
+        assertTrue("casa".equalsIgnoreCase("CASA"));
+        assertTrue("Casa".endsWith("sa"));
+        assertTrue("Casa".startsWith("Ca"));
+
+        List<String> s1 = new ArrayList<>();
+        List<String> s2 = new ArrayList<>();
+        List<String> s3 = null;
+
+        assertEquals(s1, s2);
+        assertSame(s1, s1); //vai olhar se é exatamente a mesma referência
+        assertNotEquals(s1,s3);
+        assertNull(s3);
+        assertNotNull(s1);
+//        fail("Falhou por nenhum motivo"); // serve somente para adicionar uma falha
     }
 }
