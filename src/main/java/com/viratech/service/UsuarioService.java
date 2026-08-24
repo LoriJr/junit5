f6af38d9cb00c513471050cdd1a7335607a0dbef
lorijr <jrsilva.lorivas@gmail.com>
2026-08-24 15:28:35 -0300

    implementado metodo para salvar usuario

--- src/main/java/com/viratech/service/UsuarioService.java
+++ src/main/java/com/viratech/service/UsuarioService.java
@@ -1,6 +1,17 @@
 package com.viratech.service;
 
+import com.viratech.domain.Usuario;
+import com.viratech.service.repositories.UsuarioRepository;
+
 public class UsuarioService {
 
-     
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario salvarUsuario(Usuario usuario){
+         return usuarioRepository.salvar(usuario);
+     }
 }
