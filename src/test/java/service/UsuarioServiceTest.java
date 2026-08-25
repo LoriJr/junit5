463f6977d254dde1a771df6eb83a1bc6576157c4
lorijr <jrsilva.lorivas@gmail.com>
2026-08-25 11:57:36 -0300

    adicionando MockitoExtension e teste

--- src/test/java/service/UsuarioServiceTest.java
+++ src/test/java/service/UsuarioServiceTest.java
@@ -7,26 +7,45 @@ import domain.builders.UsuarioBuilder;
 import infra.UsuarioDummyRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class UsuarioServiceTest {
 
+    @Mock
+    private UsuarioRepository repository;
+
+    @InjectMocks
     private UsuarioService service;
 
-    @BeforeEach
-    void setUp(){
-        UsuarioRepository repository = Mockito.mock(UsuarioRepository.class);
-        service = new UsuarioService(repository);
+    @Test
+    public void deveRetornarEmptyQuandoUsuarioInexistente(){
+       Optional<Usuario> usuario = service.getUserByEmail("email@gmail.com");
+       assertTrue(usuario.isEmpty());
     }
 
     @Test
-    public void deveRetornarUsuarioPorEmail(){
-       Optional<Usuario> usuario = service.getUserByEmail("email@gmail.com");
-       assertTrue(usuario.isEmpty());
+    public void deveRetonarUsuarioComSucesso(){
+
+        Usuario usuario = UsuarioBuilder.umUsuario().agora();
+
+        when(repository.getUserByEmail("email@gmail.com"))
+                .thenReturn(Optional.of(usuario));
+
+        Optional<Usuario> resultado =
+                service.getUserByEmail("email@gmail.com");
+
+        assertTrue(resultado.isPresent());
+        assertEquals(usuario, resultado.get());
     }
 }
