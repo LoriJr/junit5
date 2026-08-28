1edf472d53a03acb7d98739e70d8afa5efa9226a
lorijr <jrsilva.lorivas@gmail.com>
2026-08-28 11:31:29 -0300

    adicionado teste deve rejeitar uma transacao incompleta

--- src/test/java/service/TransacaoServiceTest.java
+++ src/test/java/service/TransacaoServiceTest.java
@@ -1,17 +1,25 @@
 package service;
 
+import com.viratech.domain.Conta;
 import com.viratech.domain.Transacao;
+import com.viratech.domain.exceptions.ValidationException;
 import com.viratech.service.TransacaoService;
 import com.viratech.service.repositories.TransacaoDao;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+import static domain.builders.ContaBuilder.umaConta;
 import static domain.builders.TransacaoBuilder.umTransacao;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -46,5 +54,25 @@ public class TransacaoServiceTest {
 
 
                 });
+    }
+
+
+    @ParameterizedTest(name = "{6}")
+    @MethodSource("dataProvider")
+    public void deveRejeitarTransacao(Long id, String descricao, Double valor, LocalDate data, Conta conta, Boolean status, String mensagem){
+
+        Transacao transacaoParaSalvar = umTransacao().comId(id).comDescricao(descricao).comValor(valor).comData(data).comConta(conta).comStatus(status).agora();
+        ValidationException ex = assertThrows(ValidationException.class,
+                ()-> service.salvar(transacaoParaSalvar));
+        assertEquals(mensagem, ex.getMessage());
+    }
+
+    private static Stream<Arguments> dataProvider(){
+        return Stream.of(
+                Arguments.of(1L, null, 10.0, LocalDate.now(), umaConta().agora(), true, "Descrição inexistente"),
+                Arguments.of(1L, "Transacao Valida", null, LocalDate.now(), umaConta().agora(), true, "Valor inexistente"),
+                Arguments.of(1L, "Transacao Valida", 10.0, null, umaConta().agora(), true, "Data inexistente"),
+                Arguments.of(1L, "Transacao Valida", 10.0, LocalDate.now(), null, true, "Conta inexistente")
+        );
     }
 }
